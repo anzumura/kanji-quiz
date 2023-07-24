8d239aa667f69abea32906a92d439e7af8c05c19
Dan H <9552296+anzumura@users.noreply.github.com>
2023-07-24 16:15:16 -0400

    renamed package kanji_tools to kt for now

---
+++ src/main/java/kt/utils/DomainException.java
@@ -0,0 +1,15 @@
+package kt.utils;
+
+/**
+ * DomainException is thrown when data loaded from files on startup is
+ * inconsistent or malformed, or when internal program logic errors occur.
+ */
+public class DomainException extends RuntimeException {
+
+  /**
+   * @param message exception message
+   */
+  public DomainException(String message) {
+    super(message);
+  }
+}
